9d732722cd2def3b0f195277614c389f28229f4d
thanh nguyen <thanhnguyen@thanhs-MacBook-Pro.local>
2018-09-15 07:19:52 +0700

    update

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value});
+    }
     render() {
         return (
         <form className="input-group">
-            <input />
+            <input 
+                placeholder="Get a five-day forecast in your favorite cities"
+                className="form-control"
+                value={this.state.term}
+                onChange={(event) => this.onInputChange(event)}
+            />
             <span className="button-group">
                 <button type="submit" className="btn btn-secondary">Search</button>
             </span>
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,37 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader", options: { minimize: true } }]
+      },
+      {
+        test: /\.(png|jpe?g)/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "./img/[name].[ext]",
+              limit: 10000
+            }
+          },
+          {
+            loader: "img-loader"
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -10,12 +41,14 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    }
-  }
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
